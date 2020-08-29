6a2110e5488207686b169501068fdb1d228d406f
Omar Jaff <omarjaff95@gmail.com>
2020-08-29 16:46:39 +0300

    desinged the first part of the head

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <div id="app" class="antialiased">
-    <div class="flex mt-12 mx-4">
-      <h1
-        class="text-gray-900 font-extrabold text-4xl leading-10 tracking-tight"
-      >
-        Get your Kemper Profiling Amp
-      </h1>
+  <div id="app" class="antialiased mx-4">
+    <div class="flex flex-col divide-y divide-gray-300 space-y-5">
+      <div class="flex mt-12">
+        <h1
+          class="text-gray-900 font-extrabold text-4xl leading-10 tracking-tight"
+        >
+          Get your Kemper Profiling Amp
+          <p
+            class="text-gray-500 font-medium mt-3.5 leading-normal tracking-tight text-xl"
+          >
+            All your favorite amps and effects, together in one little box.
+          </p>
+        </h1>
+      </div>
+      <div>something</div>
     </div>
-    <div></div>
+
     <div></div>
   </div>
 </template>
