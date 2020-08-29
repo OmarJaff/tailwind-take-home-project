2e02174ff965fa427ea6fe659e442c7e8fc57565
Omar Jaff <omarjaff95@gmail.com>
2020-08-29 16:26:30 +0300

    initail commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="antialiased">
+    <div class="flex mt-12 mx-4">
+      <h1
+        class="text-gray-900 font-extrabold text-4xl leading-10 tracking-tight"
+      >
+        Get your Kemper Profiling Amp
+      </h1>
+    </div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+}
+</script>
